0d5f23a39e81acf6d553e1e006905f509ddae9e6
Marcele <marcelecardozo@gmail.com>
2024-01-03 16:59:09 -0300

    Estilizando página de login, e deixando responsivo

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -7,11 +7,12 @@ const StyledContainer = styled.div`
   justify-content: center;
   align-items: center;
   height: 100vh;
+  background-color: #f5f5f5;
 `;
 
 const StyledForm = styled.form`
   width: 20%;
-  height: 30%;
+  height: 40%;
   padding: 20px;
   display: flex;
   flex-direction: column;
@@ -19,6 +20,16 @@ const StyledForm = styled.form`
   justify-content: space-evenly;
   border: 1px solid #ccc;
   border-radius: 8px;
+  background-color: #fff;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+
+  @media screen and (max-width: 600px) {
+    width: 50%;
+  }
+
+  @media screen and (max-width: 1200px) {
+    width: 40%;
+  }
 `;
 
 const StyledTextField = styled(TextField)`
@@ -27,9 +38,10 @@ const StyledTextField = styled(TextField)`
 
 const StyledButton = styled(Button)`
   && {
-    margin-top: 15px;
-    background-color: black;
+    margin-top: 20px;
+    background-color: #0c0c0c;
     color: white;
+    border-radius: 5px;
     &:hover {
       background-color: #333;
     }
@@ -38,13 +50,19 @@ const StyledButton = styled(Button)`
 
 const StyledTypography = styled(Typography)`
   margin-bottom: 15px;
+  display: flex;
+  align-items: center;
+  &::before {
+    content: "🔒";
+    margin-right: 10px;
+  }
 `;
 
 const Login: React.FC = () => {
   return (
     <StyledContainer>
       <StyledForm>
-        <StyledTypography variant="h4">Login</StyledTypography>
+        <StyledTypography variant="h4">LOGIN</StyledTypography>
         <StyledTextField
           id="username"
           label="Username"
